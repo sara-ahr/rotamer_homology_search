7ebb3c2d73e4ae26ab06594ef48fe8742307376b
sara-ahr <sara.7.ahrari@gmail.com>
2023-07-07 15:08:06 +0200

    Add

---
+++ alphabet_conversion_chi1.py
@@ -0,0 +1,82 @@
+import matplotlib.pyplot as plt
+import pandas as pd 
+import plotly.graph_objects as go
+import os
+from sklearn.cluster import KMeans
+import numpy as np
+import warnings
+from math import cos
+from math import sin
+import sys
+import random
+warnings.filterwarnings('ignore')
+os.chdir('/home/sarah/Downloads/Codes/')
+
+
+coordinates = np.load('cluster_centroids_chi1.npy', allow_pickle = True).item()
+
+AAs = ['ARG','ASN','ASP','CYS','GLU', 'GLN','HIS','ILE','LEU','LYS','MET','PHE','PRO','SER','THR','TRP','TYR','VAL']
+concode = dict()
+AAnum = 65
+for a,i in enumerate(AAs):
+    for x in range(3):
+        if len(chr(AAnum)) == 1 and chr(AAnum) != '\\':
+            concode[f'{i}{x}'] = chr(AAnum)
+        else:    
+            while len(chr(AAnum)) > 1 or chr(AAnum) == '\\':
+                AAnum += 1
+            concode[f'{i}{x}'] = chr(AAnum)
+        AAnum += 1
+if len(chr(AAnum)) == 1:
+    concode['miss_info'] = chr(AAnum)
+else:    
+    while len(chr(AAnum)) > 1:
+        AAnum += 1
+    concode['miss_info'] = chr(AAnum)
+
+for a in ['GLY','ALA']:
+    AAnum += 1
+    if len(chr(AAnum)) == 1:
+        concode[a] = chr(AAnum)
+    else:
+        while len(chr(AAnum)) > 1:
+            AAnum += AAnum
+        concode[a] = chr(AAnum)
+print(concode)
+
+
+    
+for f in os.listdir('/home/sarah/Downloads/pdb_torsion_angles'):
+    inputname = os.path.join('/home/sarah/Downloads/pdb_torsion_angles/', f)
+    input_file = pd.read_csv(inputname)
+    input_file = pd.DataFrame(input_file)
+    input_file_del = input_file[['resn', 'chi1']]
+    AAs = ['ARG','ASN','ASP','CYS','GLU', 'GLN','HIS','ILE','LEU','LYS','MET','PHE','PRO','SER','THR','TRP','TYR','VAL']
+
+    sequence =[]
+    for i in range(len(input_file_del)):
+        if input_file_del.iloc[i,0] not in ['GLY','ALA']:
+            query = np.array(input_file_del.iloc[i,1:])
+            query = query[~pd.isnull(query)]
+            query = query[:2]/180 * 3.1415
+            if len(query)>0: 
+                query = np.array([cos(query[0]), sin(query[0])])
+                try:
+                    label = np.argmin(np.linalg.norm(coordinates[f'centroids{input_file_del.iloc[i,0]}']-np.array([query, query, query]), axis=1))
+                    sequence.append(concode[f'{input_file_del.iloc[i,0]}{label}'])
+                except ValueError:
+                    sequence.append(concode['miss_info'])
+            else:    
+                sequence.append(concode['miss_info'])
+        else:
+            sequence.append(concode[input_file_del.iloc[i,0]])
+        
+
+    name = inputname.split('/')[-1]
+    name = name.split('.')[:-1]
+    name = '.'.join(name)
+    name = str(name[7:])
+    os.chdir('/home/sarah/Downloads/pdb_con_seqs_chi1/')
+    with open(f'con_seq_{name}', 'w') as f:
+        f.write(''.join(sequence))
+    print(f'con_seq_{name}','completed')
